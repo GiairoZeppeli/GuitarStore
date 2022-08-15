7e6ea185ff22d83a5af83198d1821ecc4ba79ce5
GiairoZeppeli <zakhar.agafonov99@mail.ru>
2022-08-15 17:41:26 +0300

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GuitarStore;
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IGuitarRepository guitarRepository;
+
+        public SearchController(IGuitarRepository guitarRepository)
+        {
+            this.guitarRepository = guitarRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var guitars = guitarRepository.GeyAllByTitle(query);
+            return View(guitars);
+        }
+    }
+}
